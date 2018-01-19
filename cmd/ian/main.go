39859ca1d1a65d3167f608ce3617db80e2aec1a2
Robert McLeod <robert@autogrow.com>
2018-01-20 02:29:14 +1300

    add a basic help page, added deps, versions and make version print pkg version

--- cmd/ian/main.go
+++ cmd/ian/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
 
 	ian "github.com/penguinpowernz/go-ian"
@@ -28,6 +29,9 @@ func main() {
 	cmd := os.Args[1]
 
 	switch cmd {
+	case "help", "-h", "--help":
+		printHelp()
+		os.Exit(0)
 	case "init":
 		fatalIf(ian.Init(dir), "")
 
@@ -46,18 +50,13 @@ func main() {
 		fmt.Println(size, "kB")
 
 	case "pkg":
-		ensureInit(dir)
-		ctrl, err := control.Read(ian.ControlFile(dir))
-		fatalIf(err, "couldn't read control file")
+		ctrl := readCtrl(dir)
 		name, err := ian.Package(ctrl, dir, "")
 		fatalIf(err, "packaging failed")
 		fmt.Println(name)
 
 	case "set":
-		ensureInit(dir)
-		fn := ian.ControlFile(dir)
-		ctrl, err := control.Read(fn)
-		fatalIf(err, "couldn't read control file")
+		ctrl := readCtrl(dir)
 
 		var newVer, newArch string
 		fs := flag.NewFlagSet("set", flag.ContinueOnError)
@@ -73,12 +72,16 @@ func main() {
 			ctrl.Version = newVer
 		}
 
-		fatalIf(ctrl.WriteFile(fn), "couldn't set fields")
+		fatalIf(ctrl.WriteFile(ian.ControlFile(dir)), "couldn't set fields")
+
+	case "deps":
+		ctrl := readCtrl(dir)
+		for _, dep := range ctrl.Depends {
+			fmt.Println(dep)
+		}
 
 	case "info":
-		ensureInit(dir)
-		ctrl, err := control.Read(ian.ControlFile(dir))
-		fatalIf(err, "couldn't read control file")
+		ctrl := readCtrl(dir)
 		fmt.Println(ctrl.String())
 
 	case "push":
@@ -89,8 +92,7 @@ func main() {
 			tell.Fatalf("No .ianpush file exists")
 		}
 
-		ctrl, err := control.Read(ian.ControlFile(dir))
-		tell.IfFatalf(err, "couldn't read control file")
+		ctrl := readCtrl(dir)
 		pkg := filepath.Join(dir, "pkg", ctrl.Filename())
 
 		if len(os.Args) == 3 {
@@ -100,14 +102,21 @@ func main() {
 		tell.IfFatalf(ian.Push(pushFile, pkg), "pushing failed")
 
 	case "-v":
-		fallthrough
-	case "version":
 		fmt.Println("Version", version)
-		fmt.Println("In memory of Ian Ashley Murdock (1973 - 2015)")
+		fmt.Println("In memory of Ian Ashley Murdock (1973 - 2015), founder of the Debian Project")
+
+	case "versions":
+		cmd := exec.Command("/usr/bin/git", "tag")
+		cmd.Stdout = os.Stdout
+		tell.IfFatalf(cmd.Run(), "")
+
+	case "version":
+		ctrl := readCtrl(dir)
+		fmt.Printf("%s: %s\n", ctrl.Name, ctrl.Version)
 
 	default:
 		fmt.Println("unknown argument:", cmd)
-		fmt.Println("Usage: ian <command> [options]")
+		printHelp()
 	}
 }
 
@@ -127,4 +136,34 @@ func ensureInit(dir string) {
 	if !ian.IsInitialized(dir) {
 		log.Fatal("not initialized")
 	}
+}
+
+func readCtrl(dir string) control.Control {
+	ensureInit(dir)
+	ctrl, err := control.Read(ian.ControlFile(dir))
+	fatalIf(err, "couldn't read control file")
+	return ctrl
+}
+
+func printHelp() {
+	fmt.Println(`Usage: ian <command> [options]
+	-v,             Print the version
+	-h, --help      Display this help message.
+
+Available commands:
+
+	new        Create a new Debian package from scratch
+	init       Initialize the current folder as a Debian package
+	pkg        Build a Debian package
+	push       Push the latest debian package up
+	set        Modify the Debian control file
+	info       Print information for this package
+	deps       Print dependencies for this package
+	versions   Show all the known versions
+	version    Print the current versions
+`)
+
+	// build      Run the script at DEBIAN/build
+	// install    Build and install a Debian package
+	// release    Release the current or new version
 }
