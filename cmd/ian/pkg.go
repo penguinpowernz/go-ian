51017dbeb69d6556c999f841721420a744284e2e
Robert McLeod <robert@penguinpower.co.nz>
2026-03-12 15:43:55 +1300

    dump the file list on build

--- cmd/ian/pkg.go
+++ cmd/ian/pkg.go
@@ -11,6 +11,8 @@ import (
 func init() {
 	pkgCmd.Flags().StringP("outpath", "o", "", "output path (the push command won't see files in this dir)")
 	pkgCmd.Flags().BoolP("debug", "x", false, "debug mode")
+	pkgCmd.Flags().BoolP("dry-run", "n", false, "print files that would be included without building")
+	pkgCmd.Flags().BoolP("quiet", "q", false, "suppress file list when building")
 	rootCmd.AddCommand(pkgCmd)
 }
 
@@ -21,8 +23,22 @@ var pkgCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		PKG = readPkg(DIR)
 
-		outpath := cmd.Flag("outpath").Value.String()
+		dryRun, _ := cmd.Flags().GetBool("dry-run")
+		quiet, _ := cmd.Flags().GetBool("quiet")
 		debug, _ := cmd.Flags().GetBool("debug")
+
+		if dryRun || (!quiet && !debug) {
+			files, err := PKG.ListFiles()
+			tell.IfFatalf(err, "failed to list package files")
+			for _, f := range files {
+				fmt.Println(f)
+			}
+			if dryRun {
+				return
+			}
+		}
+
+		outpath := cmd.Flag("outpath").Value.String()
 
 		pkgr := ian.DefaultPackager()
 		outfile, err := pkgr.BuildWithOpts(PKG, ian.BuildOpts{Outpath: outpath, Debug: debug})
